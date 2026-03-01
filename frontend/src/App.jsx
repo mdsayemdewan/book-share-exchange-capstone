fd1fd8d8dda973e0f4a8b42d17f275c77598bf25
mdsayemdewan <mdsayemdewan5240@gmail.com>
2026-03-01 20:18:21 +0600

    app-jsx update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,9 +7,6 @@ import Pending from './pages/Pending.jsx';
 import AdminDashboard from './pages/AdminDashboard.jsx';
 import UserDashboard from './pages/UserDashboard.jsx';
 import Home from './pages/Home.jsx';
-import Exchange from './pages/Exchange.jsx';
-import Share from './pages/Share.jsx';
-import RatingRanking from './pages/RatingRanking.jsx';
 export default function App() {
   return (
     <BrowserRouter>
@@ -20,23 +17,6 @@ export default function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/pending" element={<Pending />} />
-          <Route path="/rating" element={<RatingRanking />} />
-          <Route
-            path="/share"
-            element={
-              <ProtectedRoute role="user">
-                <Share />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/exchange"
-            element={
-              <ProtectedRoute role="user">
-                <Exchange />
-              </ProtectedRoute>
-            }
-          />
 
           <Route
             path="/dashboard"
