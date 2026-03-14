ac4dbcfc5bd6df0fe759ac94cec0b36a6efa7bdc
mdsayemdewan <mdsayemdewan5240@gmail.com>
2026-03-14 12:46:30 +0600

    updated app jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,16 +7,40 @@ import Pending from './pages/Pending.jsx';
 import AdminDashboard from './pages/AdminDashboard.jsx';
 import UserDashboard from './pages/UserDashboard.jsx';
 import Home from './pages/Home.jsx';
+import Exchange from './pages/Exchange.jsx';
+import Share from './pages/Share.jsx';
+import RatingRanking from './pages/RatingRanking.jsx';
+import Profile from './pages/Profile.jsx';
+
+
 export default function App() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen bg-gray-50">
+      <div className="min-h-screen">
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/pending" element={<Pending />} />
+
+          <Route path="/rating" element={<RatingRanking />} />
+          <Route
+            path="/share"
+            element={
+              <ProtectedRoute role="user">
+                <Share />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/exchange"
+            element={
+              <ProtectedRoute role="user">
+                <Exchange />
+              </ProtectedRoute>
+            }
+          />
 
           <Route
             path="/dashboard"
@@ -31,6 +55,15 @@ export default function App() {
             element={
               <ProtectedRoute role="admin">
                 <AdminDashboard />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute role="user">
+                <Profile />
               </ProtectedRoute>
             }
           />
