1846382be95f2c8a3f6f3d19700656b022330461
mdsayemdewan <mdsayemdewan5240@gmail.com>
2026-03-31 13:10:51 +0600

    deploying frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,13 @@ import userRoutes from './routes/userRoutes.js';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://book-share-exchange-capstone.vercel.app',
+    'http://localhost:5173'
+  ],
+  credentials: true
+}));
 app.use(express.json({ limit: '10mb' }));
 connectDB();
 
