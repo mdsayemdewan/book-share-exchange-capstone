2161f6214576fe67b3e09f8f4372b5e86e74c3c9
mdsayemdewan <mdsayemdewan5240@gmail.com>
2026-04-02 09:17:17 +0600

    solving map icon issue and error

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
 import './index.css'
 import App from './App.jsx'
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+  iconRetinaUrl: markerIcon2x,
+  shadowUrl: markerShadow,
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
